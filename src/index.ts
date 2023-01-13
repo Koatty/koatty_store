b1e4710bb39165f5d3e3e13075666fd0a27596c6
linyyyang <linyyyang@tencent.com>
2023-01-13 12:10:06 +0800

    build: deps

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * @Author: richen
  * @Date: 2020-11-30 11:48:12
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-12-02 16:07:55
+ * @LastEditTime: 2023-01-13 11:25:02
  * @License: BSD (3-Clause)
  * @Copyright (c) - <richenlin(at)gmail.com>
  */
@@ -20,25 +20,25 @@ export { CacheStore } from "./store";
  * @interface StoreOptions
  */
 export interface StoreOptions {
-    type?: string;
-    keyPrefix?: string;
-    host?: string | Array<string>;
-    port?: number | Array<number>;
-    username?: string;
-    password?: string;
-    db?: number;
-    timeout?: number; // seconds
-    poolSize?: number;
-    connectTimeout?: number; // milliseconds
+  type?: string;
+  keyPrefix?: string;
+  host?: string | Array<string>;
+  port?: number | Array<number>;
+  username?: string;
+  password?: string;
+  db?: number;
+  timeout?: number; // seconds
+  poolSize?: number;
+  connectTimeout?: number; // milliseconds
 
-    // sentinel
-    name?: string;
-    sentinelUsername?: string;
-    sentinelPassword?: string;
-    sentinels?: Array<{ host: string; port: number }>;
+  // sentinel
+  name?: string;
+  sentinelUsername?: string;
+  sentinelPassword?: string;
+  sentinels?: Array<{ host: string; port: number }>;
 
-    // cluster
-    clusters?: Array<{ host: string; port: number }>;
+  // cluster
+  clusters?: Array<{ host: string; port: number }>;
 }
 
 /**
@@ -48,42 +48,42 @@ export interface StoreOptions {
  * @class Store
  */
 export class Store {
-    private static instance: CacheStore;
+  private static instance: CacheStore;
 
-    /**
-     * 
-     *
-     * @static
-     * @returns
-     * @memberof ValidateUtil
-     */
-    static getInstance(options: StoreOptions): CacheStore {
-        if (this.instance) {
-            return this.instance;
-        }
-        options = {
-            ...{
-                type: 'memory', // memory | redis
-                host: '',
-                port: 0,
-                keyPrefix: 'Koatty',
-                timeout: 600,
-                poolSize: 10,
-                connectTimeout: 500,
-                db: 0
-            }, ...options
-        };
-        switch (options.type) {
-            case "redis":
-                this.instance = new RedisStore(options);
-                break;
-            case "memory":
-            default:
-                this.instance = new MemoryStore(options);
-                break;
-        }
-
-        return this.instance;
+  /**
+   * 
+   *
+   * @static
+   * @returns
+   * @memberof ValidateUtil
+   */
+  static getInstance(options: StoreOptions): CacheStore {
+    if (this.instance) {
+      return this.instance;
     }
+    options = {
+      ...{
+        type: 'memory', // memory | redis
+        host: '',
+        port: 0,
+        keyPrefix: 'Koatty',
+        timeout: 600,
+        poolSize: 10,
+        connectTimeout: 500,
+        db: 0
+      }, ...options
+    };
+    switch (options.type) {
+      case "redis":
+        this.instance = new RedisStore(options);
+        break;
+      case "memory":
+      default:
+        this.instance = new MemoryStore(options);
+        break;
+    }
+
+    return this.instance;
+  }
 }
 
